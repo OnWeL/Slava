fa7c66e57115b9aca20738d74a1980aadbde447c
OnWeL <krailooleg8888@gmail.com>
2020-06-21 11:50:55 +0300

    First

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    var wick=0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Air("Expert")
+    }
+
+    fun Air(Bund:String){
+        Toast.makeText(this,Bund,Toast.LENGTH_LONG)
+        wick=0
+    }
+
+}
